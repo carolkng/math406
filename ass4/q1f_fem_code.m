1c534a8041812e17f8ddfadb2bd9df396f27c92e
Carol Ng <carol@carolkng.com>
2018-11-09 06:38:29 -0800

    hm'

--- ass4/q1f_fem_code.m
+++ ass4/q1f_fem_code.m
@@ -45,7 +45,7 @@ e = [1;e];
 % Finite difference matrix
 F = 1/h^2 * spdiags([e -2*e e], -1:1, N+1, N+1);
 % u(0) = 0
-F(1,1) = 0;
+F(1,1) = 1;
 F(1,2) = 0;
 % Neumann BC at u(1) 
 F(N+1,N) = -1/h;
@@ -53,16 +53,19 @@ F(N+1,N+1) = 1/h;
 
 A = spdiags([e], 0, N+1, N+1);
 % Neumann BC at u(1)
-A(1,1) = 0;
+A(1,1) = alpha;
+A(N,N) = beta;
 
-[V_fd,D_fd] = eig(full(F), -1*full(A));
+[V_fd,D_fd] = eig(-full(F), full(A));
 
 disp(diag(D_fem)')
 disp(diag(D_fd)')
 
 % Plot results of exact solutions
 x_smooth = 0:0.005:1;
-plot(x_smooth, eigf1(x_smooth), x_smooth, eigf2(x_smooth), x_smooth, eigf3(x_smooth));
+plot(x_smooth, eigf1(x_smooth), '-k');
+plot(x_smooth, eigf2(x_smooth), '-.k');
+plot(x_smooth, eigf3(x_smooth), '--k');
 
 % Plot results of FEM
 % plot(x_vec, [0; V_fem(:,1)], 'o');
@@ -72,12 +75,15 @@ plot(x_smooth, eigf1(x_smooth), x_smooth, eigf2(x_smooth), x_smooth, eigf3(x_smo
 % Scale the solutions by an appropriate factor:
 x_vec = linspace(x0, x1, N+1);
 [~, max_ind] = max(abs(V_fem(:,1)));
-plot(x_vec, [0; V_fem(:,1) / V_fem(max_ind,1)], 'o');
+plot(x_vec, [0; V_fem(:,1) / V_fem(max_ind,1)], 'ok');
 [~, max_ind] = max(abs(V_fem(:,1)));
-plot(x_vec, [0; -1*V_fem(:,2) / V_fem(max_ind,2)], 'o');
+plot(x_vec, [0; -1*V_fem(:,2) / V_fem(max_ind,2)], 'ok');
 [~, max_ind] = max(abs(V_fem(:,1)));
-plot(x_vec, [0; V_fem(:,3) / V_fem(max_ind,3)], 'o');
+plot(x_vec, [0; V_fem(:,3) / V_fem(max_ind,3)], 'ok');
 
-% Plot results of FD approximation:
+% Plot results of FD approximation, which is hella wrong:
+plot(x_vec, V_fd(:,1), '-*b');
+plot(x_vec, V_fd(:,2), '-xb');
+plot(x_vec, V_fd(:,3), '-db');
 
 legend("Exact: V1", "Exact: V2", "Exact: V3", "FEM approx: V1", "FEM approx: V2", "FEM approx: V3");
